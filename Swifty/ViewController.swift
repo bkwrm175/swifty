3592e174bde6d961ed46319bd2d8d569c18edbdc
Tanner Watt <tannerwatt@Tanners-MacBook-Pro.local>
2017-01-30 21:02:19 -0700

    Changed to 120

--- Swifty/ViewController.swift
+++ Swifty/ViewController.swift
@@ -3,16 +3,30 @@
 //  Swifty
 //
 //  Created by Tanner Watt on 2017-01-22.
-//  Copyright © 2017 Tanner Watt. All rights reserved.
+//  Copyright © 2017 Tanner Watt. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label1: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func ButtonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount >= 12{
+            Label1.text = "Stop Tapping Me"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
